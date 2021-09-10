e318b92f962138e45d84097875cc07faa6eecf1b
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-09 22:30:15 -0300

    itens opcionais

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
+// interface IUsuario {
+//     id: string;
+//     email: string;
+// }
+
+// interface IAdmin extends IUsuario {
+//     cargo: 'gerente' | 'coordenador' | 'supervisor';
+// }
+
+// function redirecione(usuario: IUsuario | IAdmin) {
+//     if ('cargo' in usuario) {
+//         // redirecione para a area de administração
+//     }
+//         // redirecione para a area de usuario
+// }
+
 interface IUsuario {
     id: string;
     email: string;
+    cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
 }
 
-interface IAdmin extends IUsuario {
-    cargo: 'gerente' | 'coordenador' | 'supervisor';
-}
-
-function redirecione(usuario: IUsuario | IAdmin) {
-    if ('cargo' in usuario) {
+function redirecione(usuario: IUsuario) {
+    if (usuario.cargo) {
         // redirecione para a area de administração
     }
         // redirecione para a area de usuario
