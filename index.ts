c260e1da3dda8fa2503e8390195b2960e1a4ee0b
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-09 21:30:34 -0300

    define type

--- index.ts
+++ index.ts
@@ -15,27 +15,45 @@ interface Animal {
     nome: string;
     tipo: 'terrestre' | 'aquatico';
     som(decibeis: number): void;
+    domestico: boolean;
 }
 
 interface Felinos extends Animal {
     visaoNoturna: boolean;
 }
 
+interface Canino extends Animal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type Domestico = Felinos | Canino;
+
 const animal: Animal = {
     nome: 'Elefante',
     tipo: 'terrestre',
-    som: (decibeis) => (`${decibeis}db`)
+    som: (decibeis) => (`${decibeis}db`),
+    domestico: false,
 }
 
-animal.som('1');
+// animal.som('1');
 animal.som(3);
 
 const gatenhos: Felinos = {
     nome: 'gato',
     tipo: 'terrestre',
     visaoNoturna: true,
-    som: (decibeis) => (`${decibeis}db`)
+    som: (decibeis) => (`${decibeis}db`),
+    domestico: true,
 }
 
-gatenhos.som('1');
+// gatenhos.som('1');
 gatenhos.som(3);
+
+const bicho: Domestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'pequeno',
+    tipo: 'terrestre',
+    som: (decibeis) => (`${decibeis}db`),
+    // visaoNoturna: true,  
+}
