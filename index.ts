33d0861c924d96f0c23eefd434bad4e5a4d03c8d
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-09 21:07:07 -0300

    define interface extendida

--- index.ts
+++ index.ts
@@ -4,3 +4,27 @@ function some(a: number, b: number) {
 
 console.log(some(1,4))
 console.log(some(1, '2'))
+
+// types
+
+
+// interfaces
+interface Animal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+}
+
+interface Felinos extends Animal {
+    visaoNoturna: boolean;
+}
+
+const animal: Animal = {
+    nome: 'Elefante',
+    tipo: 'terrestre'
+}
+
+const gatenhos: Felinos = {
+    nome: 'gato',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+}
