1d7904b6fefd62164ab689d3ed5e3916e35ef036
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-09 22:23:10 -0300

    condicional com parametros

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
-const input = document.getElementById('input') as HTMLInputElement;
-
-input.addEventListener('input', (event) => {
-    const digitou = event.currentTarget as HTMLInputElement;
-    console.log(digitou.value)
-})
-
-// Generic Types
-
-function preencheLista<T>(array: T[], valor: T) {
-    return array.map(() => valor);
+interface IUsuario {
+    id: string;
+    email: string;
 }
 
-preencheLista([1, 2, 3], 4);
+interface IAdmin extends IUsuario {
+    cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
 
-// T pode ser qualquer tipo (any)
+function redirecione(usuario: IUsuario | IAdmin) {
+    if ('cargo' in usuario) {
+        // redirecione para a area de administração
+    }
+        // redirecione para a area de usuario
+}
